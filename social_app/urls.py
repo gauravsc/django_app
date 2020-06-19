d0d0836cfc3933644201c091ae07100186041c39
Your Name <you@example.com>
2020-06-19 10:59:24 +0100

    first commit

--- social_app/urls.py
+++ social_app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import list_view, logout_view
+from .views import list_view, logout_view, post_submission
 
 urlpatterns = [
 	path('stories', list_view),
-	path('logout', logout_view)
+	path('logout', logout_view),
+	path('post_submission', post_submission)
 ]
     
--- social_app/views.py
+++ social_app/views.py
@@ -1,14 +1,33 @@
 from django.shortcuts import render
 from django.contrib.auth import logout
+from django.contrib.auth.models import User
+from social_app.models import Spreadsheets
+from datetime import datetime
 
 # Create your views here.
 
 def list_view(request):
 	context = {}
 
-	context['data'] = [1, 2, 3]
+	context['current_spreadsheet'] = "{'name':'Gaurav'}"
 
 	return render(request, "social_app/list_view.html", context)
+
+
+def post_submission(request):
+	if request.method == 'POST':
+		spreadsheet = request.POST['spreadsheet']
+
+	data_to_save = Spreadsheets(user=request.user,
+		upvotes=2,
+		spreadsheet_json=spreadsheet,
+		pub_date=datetime.now()
+		)
+	data_to_save.save()
+
+	return render(request, "social_app/thank.html")
+
+
 
 def logout_view(request):
 	logout(request)
